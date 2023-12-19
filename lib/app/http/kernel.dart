10cdfbf92caf72c868b4e9feb6c2aacb7c3babe9
Chima Precious <ositachima.co@gmail.com>
2023-12-19 13:54:08 +0000

    no need for kernel override

--- lib/app/http/kernel.dart
+++ lib/app/http/kernel.dart
@@ -1,13 +1,3 @@
-import 'package:yaroo/http/http.dart';
 import 'package:yaroo/http/kernel.dart' as prefix01;
 
-class Kernel extends prefix01.Kernel {
-  @override
-  List<Middleware> get $middleware => [];
-
-  @override
-  Map<String, List<Middleware>> get $middlewareAliases => {};
-
-  @override
-  Map<String, List<Middleware>> get $middlewareGroups => {};
-}
+class Kernel extends prefix01.Kernel {}
