d11ca63bb0221696a20ccdb39e333ac9d4bfd4c0
Chima Precious <ositachima.co@gmail.com>
2024-01-02 14:34:57 +0000

    fix 404 page

--- lib/src/controllers/user_controller.dart
+++ lib/src/controllers/user_controller.dart
@@ -4,8 +4,6 @@ import 'package:yaroorm/yaroorm.dart';
 import 'package:zomato/src/models/models.dart';
 import 'package:zomato/src/services/services.dart';
 
-import '../models/dto/user_dto.dart';
-
 class UserController extends HTTPController {
   final UserService userSvc;
 
@@ -13,38 +11,12 @@ class UserController extends HTTPController {
 
   Future<Response> index() async {
     final result = await DB.query<User>().all();
-
     return jsonResponse(result);
   }
 
-  Future<Response> create(@body CreateUserDTO userDTO) async {
-    final user = await User(userDTO.firstname, userDTO.lastname, userDTO.age).save();
-
-    return jsonResponse(user);
-  }
-
   Future<Response> show(@param int userId) async {
-    final user = await DB.query<User>().get(userId);
+    final user = await userSvc.getUser(userId);
     if (user == null) return notFound('User not found');
-
     return jsonResponse(user);
-  }
-
-  Future<Response> update(@param int userId, @body UpdateUserDTO reqBody) async {
-    var user = await DB.query<User>().get(userId);
-    if (user == null) return notFound('User not found');
-
-    user = await user.update(reqBody.data);
-
-    return jsonResponse(user);
-  }
-
-  Future<Response> delete(@param int userId) async {
-    final user = await DB.query<User>().get(userId);
-    if (user == null) return notFound('User not found');
-
-    await user.delete();
-
-    return jsonResponse('User $userId deleted successfully');
   }
 }
