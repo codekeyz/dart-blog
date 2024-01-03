c91de210b3d45f9052de66b567601e256800861c
Chima Precious <ositachima.co@gmail.com>
2024-01-03 13:28:25 +0000

    add tests for auth controller

--- test/zomato_test.dart
+++ test/zomato_test.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:test/test.dart';
 import 'package:yaroorm/yaroorm.dart';
 import 'package:backend/app/app.dart';
@@ -7,14 +10,105 @@ import '../config/database.dart' as db;
 
 import 'zomato_test.reflectable.dart';
 
-final backend = App(app.config);
+final server = App(app.config);
 
 void main() {
   initializeReflectable();
 
   DB.init(db.config);
 
-  setUpAll(() => backend.bootstrap(listen: false));
+  setUpAll(() => server.bootstrap(listen: false));
 
-  group('Zomato API Tests', () {});
+  group('Zomato API Tests', () {
+    const baseAPIPath = '/api';
+
+    group('Auth', () {
+      const authPath = '$baseAPIPath/auth';
+      group('/register', () {
+        final path = '$authPath/register';
+        test('should error on invalid body', () async {
+          attemptRegister(Map<String, dynamic> body, {dynamic errors}) async {
+            return (await server.tester)
+                .post(path, body)
+                .expectStatus(HttpStatus.badRequest)
+                .expectJsonBody({'location': 'body', 'errors': errors}).test();
+          }
+
+          // when empty body
+          await attemptRegister({}, errors: [
+            'name: The field is required',
+            'email: The field is required',
+            'password: The field is required',
+          ]);
+
+          // when only name provide
+          await attemptRegister({
+            'name': 'Foo'
+          }, errors: [
+            'email: The field is required',
+            'password: The field is required',
+          ]);
+
+          // when invalid email
+          await attemptRegister({
+            'name': 'Foo',
+            'email': 'bar'
+          }, errors: [
+            'email: The field is not a valid email address',
+            'password: The field is required',
+          ]);
+
+          // when no password
+          await attemptRegister(
+            {'name': 'Foo', 'email': 'foo@bar.com'},
+            errors: ['password: The field is required'],
+          );
+
+          // when short password
+          await attemptRegister(
+            {'name': 'Foo', 'email': 'foo@bar.com', 'password': '344'},
+            errors: ['password: The field must be at least 8 characters long'],
+          );
+        });
+
+        test('should create user', () async {
+          final newUserEmail = 'foo-${DateTime.now().millisecondsSinceEpoch}@bar.com';
+          final apiResult = await (await server.tester)
+              .post(path, {'name': 'Foo User', 'email': newUserEmail, 'password': 'foo-bar-mee-moo'}).actual;
+
+          // expect json response
+          expect(apiResult.headers[HttpHeaders.contentTypeHeader], 'application/json; charset=utf-8');
+
+          // validate api result
+          final user = jsonDecode(apiResult.body)['user'];
+          expect(user['email'], newUserEmail);
+          expect(user['name'], 'Foo User');
+          expect(user, allOf(contains('id'), contains('createdAt'), contains('updatedAt')));
+        });
+      });
+
+      group('/login', () {
+        test('should error on invalid body', () async {
+          attemptLogin(Map<String, dynamic> body, {dynamic errors}) async {
+            return (await server.tester)
+                .post('$authPath/login', body)
+                .expectStatus(HttpStatus.badRequest)
+                .expectJsonBody({'location': 'body', 'errors': errors}).test();
+          }
+
+          // when empty body
+          await attemptLogin({}, errors: ['email: The field is required', 'password: The field is required']);
+
+          // when no password
+          await attemptLogin({'email': 'foo-bar@hello.com'}, errors: ['password: The field is required']);
+
+          // when invalid email
+          await attemptLogin(
+            {'email': 'foo-bar'},
+            errors: ['email: The field is not a valid email address', 'password: The field is required'],
+          );
+        });
+      });
+    });
+  });
 }
