1aaa11a68c1801050b9594de43cecb27770ad71f
Chima Precious <ositachima.co@gmail.com>
2024-01-03 11:59:03 +0000

    setup frontend project

---
+++ frontend/lib/pages/home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaffoldPage();
+  }
+}
