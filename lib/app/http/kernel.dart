5298f698f28c375be2dfca84f058fe2d4f057ee2
Chima Precious <ositachima.co@gmail.com>
2024-01-05 22:23:10 +0000

    tiny fix

--- lib/app/http/kernel.dart
+++ lib/app/http/kernel.dart
@@ -1,7 +1,7 @@
-import 'package:backend/app/http/middlewares/core_middleware.dart';
 import 'package:yaroo/http/kernel.dart' as prefix01;
 
 import 'middlewares/api_auth_middleware.dart';
+import 'middlewares/core_middleware.dart';
 import 'middlewares/serve_static_middleware.dart';
 
 class Kernel extends prefix01.Kernel {
