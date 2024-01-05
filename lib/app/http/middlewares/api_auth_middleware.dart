c3ea980cd9010cf8607108ae75cbcedb2cd690c0
Chima Precious <ositachima.co@gmail.com>
2024-01-05 03:48:08 +0000

    use user service for resolving user

--- lib/app/http/middlewares/api_auth_middleware.dart
+++ lib/app/http/middlewares/api_auth_middleware.dart
@@ -1,19 +1,18 @@
-import 'package:backend/src/models/models.dart';
 import 'package:backend/src/services/services.dart';
 import 'package:yaroo/http/http.dart';
-import 'package:yaroorm/yaroorm.dart';
 
 class ApiAuthMiddleware extends Middleware {
   final AuthService _authService;
+  final UserService _userService;
 
-  ApiAuthMiddleware(this._authService);
+  ApiAuthMiddleware(this._userService, this._authService);
 
   @override
   handle(Request req, Response res, NextFunction next) async {
     final userId = _authService.validateRequest(req);
     if (userId == null) return next(res.unauthorized());
 
-    final user = await DB.query<User>().get(userId);
+    final user = await _userService.getUser(userId);
     if (user == null) return next(res.unauthorized());
 
     return next(req..auth = user);
