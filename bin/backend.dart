27a9bf4316817b00c88321dea64877ed76291670
Chima Precious <ositachima.co@gmail.com>
2024-01-05 17:41:55 +0000

    name change

--- bin/backend.dart
+++ bin/backend.dart
@@ -5,12 +5,12 @@ import '../config/app.dart' as app;
 import '../config/database.dart' as db;
 import 'backend.reflectable.dart';
 
-final zomatoApp = App(app.config);
+final blogApp = App(app.config);
 
 void main(List<String> arguments) async {
   initializeReflectable();
 
   DB.init(db.config);
 
-  await zomatoApp.bootstrap();
+  await blogApp.bootstrap();
 }
