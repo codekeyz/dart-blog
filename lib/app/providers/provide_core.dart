23fde3e70568525b13ae4d05caafc13df8f86f07
Chima Precious <ositachima.co@gmail.com>
2024-01-06 19:35:05 +0000

    setup custom application logger

--- lib/app/providers/provide_core.dart
+++ lib/app/providers/provide_core.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:backend/src/services/services.dart';
 import 'package:yaroo/http/http.dart';
+import 'package:yaroo/http/meta.dart';
 
 class CoreProvider extends ServiceProvider {
   @override
@@ -9,5 +13,16 @@ class CoreProvider extends ServiceProvider {
     app.singleton<CookieOpts>(cookieConfig);
 
     app.singleton<AuthService>(AuthService(app.config.key, app.config.url));
+
+    app.useErrorHandler(_applicationErrorHandler);
+  }
+
+  FutureOr<Response> _applicationErrorHandler(Object error, ReqRes reqRes) async {
+    final response = reqRes.res;
+    if (error is RequestValidationError) {
+      return response.json(error.errorBody, statusCode: HttpStatus.badRequest);
+    }
+
+    return response.internalServerError(error.toString());
   }
 }
