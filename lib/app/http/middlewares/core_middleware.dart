9359f6f243d928553fdcf93ec57b3910bacb406b
Chima Precious <ositachima.co@gmail.com>
2024-01-06 19:12:36 +0000

    remove shelf helmet

--- lib/app/http/middlewares/core_middleware.dart
+++ lib/app/http/middlewares/core_middleware.dart
@@ -1,5 +1,6 @@
+import 'dart:io';
+
 import 'package:shelf_cors_headers/shelf_cors_headers.dart';
-import 'package:shelf_helmet/shelf_helmet.dart';
 import 'package:yaroo/http/http.dart';
 
 class CoreMiddleware extends Middleware {
@@ -10,15 +11,15 @@ class CoreMiddleware extends Middleware {
     final cookieConfig = app.instanceOf<CookieOpts>();
     final cookieParserMdw = cookieParser(opts: cookieConfig);
 
-    // setup helmet
-    final shelfHelmet = useShelfMiddleware(helmet());
-
     // setup cors
     final corsMiddleware = useShelfMiddleware(corsHeaders(
-      headers: {ACCESS_CONTROL_ALLOW_ORIGIN: 'http://localhost:58690/', ACCESS_CONTROL_ALLOW_CREDENTIALS: 'true'},
+      headers: {
+        HttpHeaders.accessControlAllowOriginHeader: '*',
+        HttpHeaders.accessControlAllowCredentialsHeader: 'true',
+      },
     ));
 
-    _webMdw = shelfHelmet.chain(corsMiddleware).chain(cookieParserMdw);
+    _webMdw = corsMiddleware.chain(cookieParserMdw);
   }
 
   @override
