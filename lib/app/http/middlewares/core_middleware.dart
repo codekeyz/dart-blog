3401f28da78340800aab11fd5dc0ac697aff4a98
Chima Precious <ositachima.co@gmail.com>
2024-01-06 15:10:53 +0000

    fix tests

--- lib/app/http/middlewares/core_middleware.dart
+++ lib/app/http/middlewares/core_middleware.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:shelf_cors_headers/shelf_cors_headers.dart';
 import 'package:shelf_helmet/shelf_helmet.dart';
 import 'package:yaroo/http/http.dart';
@@ -17,10 +15,7 @@ class CoreMiddleware extends Middleware {
 
     // setup cors
     final corsMiddleware = useShelfMiddleware(corsHeaders(
-      headers: {
-        HttpHeaders.accessControlAllowOriginHeader: 'http://localhost:57448',
-        HttpHeaders.accessControlAllowCredentialsHeader: 'true',
-      },
+      headers: {ACCESS_CONTROL_ALLOW_ORIGIN: 'http://localhost:58690/', ACCESS_CONTROL_ALLOW_CREDENTIALS: 'true'},
     ));
 
     _webMdw = shelfHelmet.chain(corsMiddleware).chain(cookieParserMdw);
--- lib/src/services/article_service.dart
+++ lib/src/services/article_service.dart
@@ -1,6 +1,12 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:isolate';
+
 import 'package:backend/src/models/dto/article_dto.dart';
 import 'package:backend/src/models/models.dart';
+import 'package:yaroo/yaroo.dart';
 import 'package:yaroorm/yaroorm.dart';
+import 'package:http/http.dart' as http;
 
 class ArticleService {
   Future<List<Article>> getArticles({String? ownerId}) async {
@@ -12,10 +18,12 @@ class ArticleService {
 
   Future<Article?> getArticle(int articleId) => DB.query<Article>().get(articleId);
 
-  Future<Article> createArticle(User user, CreateArticleDTO data) async {
+  Future<Article> createArticle(User user, CreateArticleDTO data, {String? imageUrl}) async {
+    imageUrl ??= await getRandomImage(data.title);
+
     final query = DB.query<Article>();
     final userId = user.id!;
-    final article = Article(userId, data.title, data.description, imageUrl: data.imageUrl);
+    final article = Article(userId, data.title, data.description, imageUrl: imageUrl);
 
     return await query.insert<Article>(article);
   }
@@ -31,4 +39,16 @@ class ArticleService {
   }
 
   Future<void> deleteArticle(int userId, int articleId) => DB.query<Article>().whereEqual('id', articleId).delete();
+
+  Future<String?> getRandomImage(String searchText) async {
+    try {
+      final response = await http.get(
+        Uri.parse('https://api.pexels.com/v1/search?query=$searchText&per_page=1'),
+        headers: {HttpHeaders.authorizationHeader: env<String>('PEXELS_API_KEY')!},
+      ).timeout(const Duration(seconds: 2));
+      final result = await Isolate.run(() => jsonDecode(response.body)) as Map;
+      return result['photos'][0]['src']['medium'];
+    } catch (_) {}
+    return null;
+  }
 }
