4d1c85b95a9ab5157205722de4a8dfa261110ac6
Chima Precious <ositachima.co@gmail.com>
2024-01-11 11:56:35 +0000

    add logging middleware

--- lib/app/http/kernel.dart
+++ lib/app/http/kernel.dart
@@ -5,13 +5,14 @@ import 'package:yaroo/http/http.dart';
 import 'package:yaroo/http/kernel.dart' as prefix01;
 import 'package:yaroo/http/meta.dart';
 
+import 'middlewares/serve_static_middleware.dart';
 import 'middlewares/api_auth_middleware.dart';
 import 'middlewares/core_middleware.dart';
-import 'middlewares/serve_static_middleware.dart';
+import 'middlewares/logger_middleware.dart';
 
 class Kernel extends prefix01.Kernel {
   @override
-  List<Type> get middleware => [CoreMiddleware];
+  List<Type> get middleware => [CoreMiddleware, LoggerMiddleware];
 
   @override
   Map<String, List<Type>> get middlewareGroups => {
