181e272cecfb4f2832f67d0ed738a176abcd967e
Chima Precious <ositachima.co@gmail.com>
2024-03-02 14:28:37 +0000

    re-add frontend project

---
+++ frontend/lib/data/providers/article_provider.dart
@@ -0,0 +1,45 @@
+import 'package:frontend/data/models/article.dart';
+import 'package:frontend/data/services.dart';
+import 'package:frontend/utils/provider.dart';
+import 'package:meta/meta.dart';
+
+class ArticleProvider extends BaseProvider<List<Article>> {
+  @visibleForTesting
+  ApiService get apiSvc => getIt.get<ApiService>();
+
+  Future<void> fetchArticles() async {
+    final articles = await safeRun(() => apiSvc.getArticles());
+    if (articles == null) return;
+
+    addEvent(ProviderEvent.success(data: articles));
+  }
+
+  Future<void> addArticle(String title, String description, String? imageUrl) async {
+    final articles = lastEvent?.data ?? [];
+    final article = await safeRun(() => apiSvc.createArticle(title, description, imageUrl));
+    if (article == null) return;
+
+    addEvent(ProviderEvent.success(data: [...articles, article]));
+  }
+
+  Future<void> updateArticle(int articleId, String title, String description, String? imageUrl) async {
+    final articles = lastEvent?.data ?? [];
+    final article = await safeRun(() => apiSvc.updateArticle(articleId, title, description, imageUrl));
+    if (article == null) return;
+
+    addEvent(ProviderEvent.success(data: [...articles, article]));
+  }
+
+  Future<void> deleteArticle(
+    int articleId,
+  ) async {
+    final articles = lastEvent?.data ?? [];
+    await safeRun(() => apiSvc.deleteArticle(
+          articleId,
+        ));
+
+    addEvent(ProviderEvent.success(data: [
+      ...articles,
+    ]));
+  }
+}
---
+++ frontend/lib/data/providers/auth_provider.dart
@@ -0,0 +1,59 @@
+import 'dart:async';
+
+import 'package:frontend/data/services.dart';
+import 'package:frontend/utils/provider.dart';
+import 'package:localstorage/localstorage.dart';
+import 'package:meta/meta.dart';
+
+import '../models/user.dart';
+
+class AuthProvider extends BaseProvider<User> {
+  @visibleForTesting
+  ApiService get apiSvc => getIt.get<ApiService>();
+
+  final LocalStorage _userLocalStore = LocalStorage('user_session_store');
+  static const String userStorageKey = 'user_data';
+
+  User? get user {
+    final userFromState = lastEvent?.data;
+    if (userFromState != null) return userFromState;
+    final serializedUser = _userLocalStore.getItem(userStorageKey);
+    return serializedUser == null ? null : User.fromJson(serializedUser);
+  }
+
+  Future<void> getUser() async {
+    if (!apiSvc.hasAuthCookie) return;
+
+    final user = await safeRun(() => apiSvc.getUser());
+    if (user == null) return;
+
+    _setUser(user);
+  }
+
+  Future<void> login(String email, String password) async {
+    final user = await safeRun(() => apiSvc.loginUser(email, password));
+    if (user == null) return;
+
+    _setUser(user);
+  }
+
+  Future<bool> register(String displayName, String email, String password) async {
+    final success = await safeRun(() => apiSvc.registerUser(displayName, email, password));
+    if (success != true) return false;
+
+    addEvent(const ProviderEvent.idle());
+    return true;
+  }
+
+  void _setUser(User user) {
+    addEvent(ProviderEvent.success(data: user));
+
+    _userLocalStore.setItem(userStorageKey, user.toJson());
+  }
+
+  void logout() {
+    apiSvc.clearAuthCookie();
+    _userLocalStore.clear();
+    addEvent(const ProviderEvent.idle());
+  }
+}
