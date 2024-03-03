4b7b23ce755671bd25e756a7f15cc7c746ed0a20
Chima Precious <ositachima.co@gmail.com>
2024-03-03 11:12:29 +0000

    simplify routes

--- lib/app/routes/web.dart
+++
@@ -1,6 +0,0 @@
-import 'package:pharaoh/next/http.dart';
-import 'package:pharaoh/next/router.dart';
-
-final routes = <RouteDefinition>[
-  Route.route(HTTPMethod.GET, '/', (req, res) => res.ok('Hello World 🤘')),
-];
