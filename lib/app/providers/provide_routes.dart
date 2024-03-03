1847c80ed000931d3f673dad7882289369c57dc8
Chima Precious <ositachima.co@gmail.com>
2024-03-03 11:12:29 +0000

    simplify routes

--- lib/app/providers/provide_routes.dart
+++ lib/app/providers/provide_routes.dart
@@ -4,35 +4,34 @@ import 'package:backend/backend.dart';
 import 'package:yaroo/http/http.dart';
 import 'package:yaroo/yaroo.dart';
 
-import '../routes/api.dart' as api;
-import '../routes/web.dart' as web;
-
 class RouteServiceProvider extends ServiceProvider {
   @override
   FutureOr<void> boot() {
     app.useRoutes(
       () => [
-        /*|--------------------------------------------------------------------------
-          | API Routes
-          |--------------------------------------------------------------------------*/
         Route.group('api', [
+          // Public Routes
           Route.post('/auth/login', (AuthController, #login)),
           Route.post('/auth/register', (AuthController, #register)),
-
           Route.get('/users/<userId>', (UserController, #show)),
-
-          /// get articles and detail without auth
           Route.group('articles', [
             Route.get('/', (ArticleController, #index)),
             Route.get('/<articleId>', (ArticleController, #show)),
           ]),
-        ]),
-        Route.middleware('api:auth').group('api', api.routes),
 
-        /*|--------------------------------------------------------------------------
-          | Web Routes
-          |--------------------------------------------------------------------------*/
-        Route.middleware('web').group('/', web.routes),
+          // Authenticated routes
+          Route.middleware('api:auth').routes([
+            Route.group('users', [
+              Route.get('/', (UserController, #index)),
+              Route.get('/me', (UserController, #currentUser)),
+            ]),
+            Route.group('articles', [
+              Route.post('/', (ArticleController, #create)),
+              Route.put('/<articleId>', (ArticleController, #update)),
+              Route.delete('/<articleId>', (ArticleController, #delete)),
+            ]),
+          ]),
+        ]),
       ],
     );
   }
--- lib/app/routes/web.dart
+++
@@ -1,6 +0,0 @@
-import 'package:yaroo/http/http.dart';
-import 'package:yaroo/yaroo.dart';
-
-final routes = <RouteDefinition>[
-  Route.route(HTTPMethod.GET, '/', (req, res) => res.ok('Hello World 🤘')),
-];
