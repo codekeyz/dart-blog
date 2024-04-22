000a19e3aa68ad64dc3a895d4a0b41558243ef12
Chima Precious <ositachima.co@gmail.com>
2024-04-22 04:22:04 +0000

    improvements

--- lib/src/services/article_service.dart
+++ lib/src/services/article_service.dart
@@ -1,9 +1,3 @@
-import 'dart:convert';
-import 'dart:io';
-import 'dart:isolate';
-
-import 'package:pharaoh/next/core.dart';
-import 'package:http/http.dart' as http;
 import 'package:yaroorm/yaroorm.dart';
 
 import '../dto/article_dto.dart';
@@ -30,17 +24,6 @@ class ArticleService {
 
   Future<Article?> getArticle(int articleId) => ArticleQuery.findById(articleId);
 
-  Future<Article> createArticle(User user, CreateArticleDTO data, {String? imageUrl}) async {
-    imageUrl ??= await getRandomImage(data.title);
-
-    return await ArticleQuery.create(
-      title: data.title,
-      description: data.description,
-      ownerId: user.id,
-      imageUrl: imageUrl,
-    );
-  }
-
   Future<Article?> updateArticle(User user, int articleId, CreateArticleDTO dto) async {
     final query = ArticleQuery.where((article) => article.and([
           article.id(articleId),
@@ -63,17 +46,5 @@ class ArticleService {
           article.id(articleId),
           article.ownerId(userId),
         ])).delete();
-  }
-
-  Future<String?> getRandomImage(String searchText) async {
-    try {
-      final response = await http.get(
-        Uri.parse('https://api.pexels.com/v1/search?query=$searchText&per_page=1'),
-        headers: {HttpHeaders.authorizationHeader: env<String>('PEXELS_API_KEY', '')},
-      ).timeout(const Duration(seconds: 2));
-      final result = await Isolate.run(() => jsonDecode(response.body)) as Map;
-      return result['photos'][0]['src']['medium'];
-    } catch (_) {}
-    return null;
   }
 }
--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -1,3 +1,10 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:isolate';
+
+import 'package:http/http.dart' as http;
+import 'package:pharaoh/next/core.dart';
+
 bool get isDebugMode {
   var isDebug = false;
   assert(() {
@@ -5,4 +12,16 @@ bool get isDebugMode {
     return true;
   }());
   return isDebug;
+}
+
+Future<String?> getRandomImage(String searchText) async {
+  try {
+    final response = await http.get(
+      Uri.parse('https://api.pexels.com/v1/search?query=$searchText&per_page=1'),
+      headers: {HttpHeaders.authorizationHeader: env<String>('PEXELS_API_KEY', '')},
+    ).timeout(const Duration(seconds: 2));
+    final result = await Isolate.run(() => jsonDecode(response.body)) as Map;
+    return result['photos'][0]['src']['medium'];
+  } catch (_) {}
+  return null;
 }
