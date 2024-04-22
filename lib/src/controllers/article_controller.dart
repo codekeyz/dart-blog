fbabe0fd6333af12891550a89acbc7beca6d7c7e
Chima Precious <ositachima.co@gmail.com>
2024-04-22 04:22:04 +0000

    improvements

--- lib/src/controllers/article_controller.dart
+++ lib/src/controllers/article_controller.dart
@@ -24,8 +24,11 @@ class ArticleController extends HTTPController {
 
   Future<Response> create(@body CreateArticleDTO data) async {
     var imageUrl = data.imageUrl;
+
     if (app.config.isDebug) {
       imageUrl ??= 'https://dart.dev/assets/shared/dart-logo-for-shares.png';
+    } else {
+      imageUrl ??= await getRandomImage(data.title);
     }
 
     final article = await user.articles.insert(NewArticleForUser(
--- lib/src/services/article_service.dart
+++ lib/src/services/article_service.dart
@@ -1,15 +1,12 @@
-import 'dart:convert';
-import 'dart:io';
 import 'dart:isolate';
 
-import 'package:pharaoh/pharaoh_next.dart';
 import 'package:yaroorm/yaroorm.dart';
-import 'package:http/http.dart' as http;
 
 import 'package:backend/src/dto/article_dto.dart';
 import 'package:backend/src/models/article.dart';
 
 import '../models/user.dart';
+import '../utils/utils.dart';
 
 class ArticleService {
   Future<List<Article>> getArticles({int? ownerId}) async {
@@ -64,17 +61,5 @@ class ArticleService {
           article.id(articleId),
           article.ownerId(userId),
         ])).delete();
-  }
-
-  Future<String?> getRandomImage(String searchText) async {
-    try {
-      final response = await http.get(
-        Uri.parse('https://api.pexels.com/v1/search?query=$searchText&per_page=1'),
-        headers: {HttpHeaders.authorizationHeader: env<String>('PEXELS_API_KEY', '')},
-      ).timeout(const Duration(seconds: 2));
-      final result = await Isolate.run(() => jsonDecode(response.body)) as Map;
-      return result['photos'][0]['src']['medium'];
-    } catch (_) {}
-    return null;
   }
 }
