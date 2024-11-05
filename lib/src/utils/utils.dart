67dcbd394c9fe4e8afd932e5a690ccc043d356fd
Chima Precious <ositachima.co@gmail.com>
2024-11-06 02:43:34 +0300

    _

--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -4,8 +4,13 @@ import 'dart:isolate';
 
 import 'package:http/http.dart' as http;
 import 'package:pharaoh/pharaoh_next.dart';
+import 'package:shared/shared.dart';
 
 Future<String?> getRandomImage(String searchText) async {
+  if (isTestMode) {
+    return 'https://dart.dev/assets/shared/dart-logo-for-shares.png';
+  }
+
   try {
     final response = await http.get(
       Uri.parse('https://api.pexels.com/v1/search?query=$searchText&per_page=1'),
