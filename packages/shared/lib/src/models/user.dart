e9e9779b40a299a598a04de6238927d6a613cc14
Chima Precious <ositachima.co@gmail.com>
2024-11-05 15:27:43 +0300

    remove entity stuff from shared model

--- packages/shared/lib/src/models/user.dart
+++ packages/shared/lib/src/models/user.dart
@@ -1,39 +1,26 @@
 import 'package:json_annotation/json_annotation.dart';
-import 'package:yaroorm/yaroorm.dart';
-
-import 'article.dart';
 
 part 'user.g.dart';
 
-@table
 @JsonSerializable(fieldRename: FieldRename.snake)
-class User extends Entity<User> {
-  @primaryKey
+class User {
   final int id;
 
   final String name;
 
   final String email;
 
-  @JsonKey(defaultValue: '', includeToJson: false)
-  final String password;
-
-  @createdAtCol
   final DateTime createdAt;
 
-  @updatedAtCol
   final DateTime updatedAt;
 
   User(
     this.id,
     this.name,
     this.email, {
-    required this.password,
     required this.createdAt,
     required this.updatedAt,
   });
-
-  HasMany<User, Article> get articles => hasMany<Article>(#articles);
 
   Map<String, dynamic> toJson() => _$UserToJson(this);
 
