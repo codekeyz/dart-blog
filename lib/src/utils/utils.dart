855cf065c76b08666cf882752eb1a61ce1ccf6b0
Chima Precious <ositachima.co@gmail.com>
2024-11-08 00:55:45 +0300

    chore: Increase timeout for getting image from pexels API

--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'dart:io';
-import 'dart:isolate';
 
 import 'package:http/http.dart' as http;
 import 'package:pharaoh/pharaoh_next.dart';
@@ -15,8 +14,8 @@ Future<String> getRandomImage(String searchText) async {
     final response = await http.get(
       Uri.parse('https://api.pexels.com/v1/search?query=$searchText&per_page=1'),
       headers: {HttpHeaders.authorizationHeader: env<String>('PEXELS_API_KEY', '')},
-    ).timeout(const Duration(seconds: 2));
-    final result = await Isolate.run(() => jsonDecode(response.body)) as Map;
+    ).timeout(const Duration(seconds: 5));
+    final result = jsonDecode(response.body) as Map;
     resultingImageUrl = result['photos'][0]['src']['medium'];
   } catch (error, trace) {
     stderr.writeln('An error occurred while getting image for $searchText');
